921925fb3dff3d3a09ee2239a02cc387c543c5f1
liuzhigui <993395848@qq.com>
2019-04-22 17:12:02 +0800

    orign

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>计数结果：{{count}}</div>
+    <a href="/pages/test1/main" class="navlink">进入计数器页面</a>
+  </div>
+</template>
+
+<script>
+  import globalStore from '../../stores/global-store'
+
+  export default {
+    computed: {
+      count () {
+        return globalStore.state.count
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .navlink {
+    text-decoration: underline;
+  }
+</style>
